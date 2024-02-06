98e37b3519f5f9273c07b97f0dde7a22350a4337
Furqan Sheikh <125441599+furqansheikh737@users.noreply.github.com>
2024-02-07 02:52:33 +0500

    create environment variable and authentication

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>A blog app with appwrite</h1>
+    </>
+  )
+}
+
+export default App
